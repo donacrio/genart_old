becbd1e12e29b7edb1f30584dd181988f209e204
Donatien Criaud <donatien.criaud@gmail.com>
2022-03-13 15:37:06 +0000

    refactor: improve performances

--- spinners/src/config.rs
+++ spinners/src/config.rs
@@ -1,18 +1,17 @@
-use config::{Config, ConfigError, File};
 use nannou::prelude::{Point2, Srgb};
 use serde::Deserialize;
 use std::path::Path;
 
-const SETTINGS_DEFAULT_PATH: &str = "configs/spinners/default.toml";
+const CONFIG_DEFAULT_PATH: &str = "configs/spinners/default.toml";
 
-pub fn load_settings(path: Option<String>) -> Settings {
+pub fn load_config(path: Option<String>) -> Config {
   let path = path.as_ref().map(|path| path.as_str()).unwrap_or_else(|| {
     println!("Could not find configuration file path. Using default configuration");
-    SETTINGS_DEFAULT_PATH
+    CONFIG_DEFAULT_PATH
   });
 
-  match Settings::new(path) {
-    Ok(settings) => settings,
+  match Config::new(path) {
+    Ok(config) => config,
     Err(err) => {
       println!("Encountered error: {}.\nProcess exited with code 1", err);
       std::process::exit(1);
@@ -21,25 +20,24 @@ pub fn load_settings(path: Option<String>) -> Settings {
 }
 
 #[derive(Debug, Deserialize)]
-pub struct WindowSettings {
+pub struct WindowConfig {
   pub background_color: Srgb<u8>,
   pub height: u32,
-  pub padding: i32,
   pub width: u32,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct SpinnerDrawingSettings {
+pub struct SpinnerDrawingConfig {
   pub color: Srgb<u8>,
   pub point_weight: f32,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct SpinnerSettings {
+pub struct SpinnerConfig {
   pub center: Point2,
   pub density: f32,
   pub density_factor: f32,
-  pub drawing: SpinnerDrawingSettings,
+  pub drawing: SpinnerDrawingConfig,
   pub initial_points: i32,
   pub radius: f32,
   pub theta_increment: f32,
@@ -47,16 +45,16 @@ pub struct SpinnerSettings {
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Settings {
-  pub iterations: usize,
-  pub window: WindowSettings,
-  pub spinners: Vec<SpinnerSettings>,
+pub struct Config {
+  pub name: String,
+  pub window: WindowConfig,
+  pub spinners: Vec<SpinnerConfig>,
 }
 
-impl Settings {
-  pub fn new(path: &str) -> Result<Settings, ConfigError> {
-    Config::builder()
-      .add_source(File::from(Path::new(path)))
+impl Config {
+  pub fn new(path: &str) -> Result<Self, config::ConfigError> {
+    config::Config::builder()
+      .add_source(config::File::from(Path::new(path)))
       .build()?
       .try_deserialize()
   }
--- spinners/src/main.rs
+++ spinners/src/main.rs
@@ -1,88 +1,64 @@
 mod cli;
-mod model;
-mod polar;
-mod settings;
+mod config;
 mod spinner;
 
 use crate::cli::parse_cli_args;
-use crate::model::{Model, WindowConfig};
+use crate::config::{load_config, Config};
 use crate::spinner::Spinner;
 use lazy_static::lazy_static;
-use nannou::app::LoopMode;
 use nannou::prelude::*;
-use settings::{load_settings, Settings};
 use std::path::Path;
 
 lazy_static! {
-    static ref SETTINGS: Settings = load_settings(parse_cli_args().config_file);
+  static ref CONFIG: Config = load_config(parse_cli_args().config_file);
 }
 
 fn main() {
-    nannou::app(model)
-        .update(update)
-        .loop_mode(LoopMode::NTimes {
-            number_of_updates: SETTINGS.iterations,
-        })
-        .run();
+  nannou::sketch(view)
+    .size(CONFIG.window.width, CONFIG.window.width)
+    .loop_mode(LoopMode::NTimes {
+      number_of_updates: 0,
+    })
+    .run();
 }
 
-fn model(app: &App) -> Model {
-    let _window = app
-        .new_window()
-        .view(view)
-        .size(SETTINGS.window.width, SETTINGS.window.height)
-        .build()
-        .unwrap();
-    let model = Model::new(_window, WindowConfig::from(&SETTINGS.window), Vec::new());
-    model
-}
+fn view(app: &App, frame: Frame) {
+  let draw = app.draw();
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    model.set_spinners(Vec::new());
-    let mut spinners: Vec<Spinner> = Vec::new();
-    for spinner_settings in &SETTINGS.spinners {
-        let spinner = Spinner::from(spinner_settings);
-        spinners.push(spinner);
+  let background_color: Srgb<f32> = CONFIG.window.background_color.into_format();
+  draw.background().color(background_color);
+
+  for spinner_config in &CONFIG.spinners {
+    let spinner = Spinner::from(spinner_config);
+    let points = spinner.compute_points();
+    let options = DrawOptions {
+      color: spinner_config.drawing.color.into_format(),
+      point_weight: spinner_config.drawing.point_weight,
+    };
+    for point in &points {
+      draw_point(&draw, point, &options);
     }
-    model.set_spinners(spinners);
-    model.update();
-    model.next_iteration();
-}
+  }
 
-fn view(app: &App, model: &Model, frame: Frame) {
-    app.window(*model._window())
-        .unwrap()
-        .capture_frame(Path::new(&format!(
-            "./frames/frame_{}.jpeg",
-            model.iteration()
-        )));
-
-    let draw = app.draw();
-
-    draw.background()
-        .color(*model.window_config().background_color());
-
-    for spinner in model.spinners() {
-        let options = DrawOptions {
-            color: *spinner.config().drawing().color(),
-            point_weight: *spinner.config().drawing().point_weight(),
-        };
-        for point in spinner.state().points() {
-            draw_point(&draw, point, &options);
-        }
-    }
-
-    draw.to_frame(app, &frame).unwrap();
+  app
+    .window(app.window_id())
+    .unwrap()
+    .capture_frame(Path::new(&format!(
+      "./frames/spinners/{}/frame.jpeg",
+      CONFIG.name
+    )));
+  draw.to_frame(app, &frame).unwrap();
 }
 
 struct DrawOptions {
-    pub color: Srgb<u8>,
-    pub point_weight: f32,
+  pub color: Srgb<u8>,
+  pub point_weight: f32,
 }
 
 fn draw_point(draw: &Draw, point: &Point2, options: &DrawOptions) {
-    draw.ellipse()
-        .xy(*point)
-        .w_h(options.point_weight, options.point_weight)
-        .color(options.color);
+  draw
+    .ellipse()
+    .xy(*point)
+    .w_h(options.point_weight, options.point_weight)
+    .color(options.color);
 }
