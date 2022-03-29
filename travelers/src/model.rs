c9ac050566ff1290de4dff593e56e433000eea08
Donatien Criaud <donatien.criaud@gmail.com>
2022-03-29 19:43:52 +0100

    feat: use texture saver for travelers

--- travelers/src/model.rs
+++ travelers/src/model.rs
@@ -1,14 +1,12 @@
 use crate::traveler::Traveler;
-use nannou::prelude::{Srgb, Vec2};
+use display::DisplayDriver;
+use nannou::prelude::Srgba;
 use std::cell::RefCell;
-use std::collections::HashMap;
-use texture::TextureSaver;
 
 pub struct Model {
-  pub _texture_saver: TextureSaver,
-  pub travelers_colors: HashMap<usize, Srgb<u8>>,
+  pub display_driver: DisplayDriver,
+  pub travelers_colors: Vec<Srgba>,
+  pub travelers_targets: Vec<usize>,
   // idea: create target trait and use it inside the travelers
-  pub travelers_target: HashMap<usize, usize>,
   pub travelers: Vec<RefCell<Traveler>>,
-  pub points: Vec<Vec2>,
 }
